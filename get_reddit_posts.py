ad558226ddd9e30507a499a5e29e440905c9c4a3
root <tblazek@nd.edu>
2020-01-16 22:18:32 +0000

    Last update of reddit post. This was done after the project was turned in

--- get_reddit_posts.py
+++ get_reddit_posts.py
@@ -52,7 +52,7 @@ class RedditParser(object):
             if info['id'] in self.posts:
                 continue
             created = info['created_utc']
-            if (now - datetime.datetime.utcfromtimestamp(created)).seconds > (60*60*8) + (20*60):
+            if (now - datetime.datetime.utcfromtimestamp(created)).seconds > (60*60*2) + (20*60):
                 continue
             post_id = info['id']
             title = info['title'].replace("\n","")
@@ -60,7 +60,7 @@ class RedditParser(object):
             url = info['url']
             score = ""
             awards = ""
-            tmp = [self.subreddit,created,post_id,title,text,url,score,awards,'False']
+            tmp = [self.subreddit,created,post_id,title,text,url,score,awards,"","","","","","","","","","","","","","","","",'False']
 
             writing.append(tmp)
         with open(self.filename,'a') as f:
@@ -75,8 +75,7 @@ class RedditParser(object):
         writing = []
         for key, value in self.posts.items():
             diff = now - datetime.datetime.utcfromtimestamp(float(value[1]))
-            print(diff, diff.seconds > (60*60*8), diff.seconds < (60*60*8)+(20*60))
-            if value[-1]=="False" and diff.seconds > (60*60*8) and diff.seconds < (60*60*8)+(20*60):
+            if value[-1]=="False" and diff.seconds > (60*60*2) and diff.seconds < (60*60*18)+(20*60):
                 update_url = "https://reddit.com/by_id/t3_" + key + ".json"
                 try:
                     reddit_response = requests.get(update_url,headers=self.headers)
@@ -89,9 +88,36 @@ class RedditParser(object):
                 post_data = reddit_data['data']['children'][0]['data']
                 new_score = post_data['score']
                 awards = post_data['total_awards_received']
-                self.posts[key][6] = new_score
-                self.posts[key][7] = awards
-                self.posts[key][-1] = 'True'
+                if diff.seconds > (60*60*2) and diff.seconds < (60*60*2) + (20*60):
+                    self.posts[key][8] = new_score
+                    self.posts[key][9] = awards
+                elif diff.seconds > (60*60*4) and diff.seconds < (60*60*4) + (20*60):
+                    self.posts[key][10] = new_score
+                    self.posts[key][11] = awards
+                elif diff.seconds > (60*60*6) and diff.seconds < (60*60*6) + (20*60):
+                    self.posts[key][12] = new_score
+                    self.posts[key][13] = awards
+                elif diff.seconds > (60*60*8) and diff.seconds < (60*60*8)+ (20*60):
+                    self.posts[key][6] = new_score
+                    self.posts[key][7] = awards
+                elif diff.seconds > (60*60*10) and diff.seconds < (60*60*10) + (20*60):
+                    self.posts[key][14] = new_score
+                    self.posts[key][15] = awards
+                elif diff.seconds > (60*60*12) and diff.seconds < (60*60*12) + (20*60):
+                    self.posts[key][16] = new_score
+                    self.posts[key][17] = awards
+                elif diff.seconds > (60*60*14) and diff.seconds < (60*60*14) + (20*60):
+                    self.posts[key][18] = new_score
+                    self.posts[key][19] = awards
+                elif diff.seconds > (60*60*16) and diff.seconds < (60*60*16) + (20*60):
+                    self.posts[key][20] = new_score
+                    self.posts[key][21] = awards
+                elif diff.seconds > (60*60*18) and diff.seconds < (60*60*18) + (20*60):
+                    self.posts[key][22] = new_score
+                    self.posts[key][23] = awards
+                    self.posts[key][-1] = 'True'
+                else:
+                    continue
         tempfile = NamedTemporaryFile(mode='w', delete=False)
         with open(self.filename, 'r') as csvfile, tempfile:
             reader = csv.DictReader(csvfile, delimiter=DELIMITER, quotechar=QUOTECHAR, fieldnames=self.csv_header)
@@ -101,8 +127,25 @@ class RedditParser(object):
                 if row['id'] in self.posts:
                     row['score'] = self.posts[row['id']][6]
                     row['awards'] = self.posts[row['id']][7]
+                    row['score_2'] = self.posts[row['id']][8]
+                    row['awards_2'] = self.posts[row['id']][9]
+                    row['score_4'] = self.posts[row['id']][10]
+                    row['awards_4'] = self.posts[row['id']][11]
+                    row['score_6'] = self.posts[row['id']][12]
+                    row['awards_6'] = self.posts[row['id']][13]
+                    row['score_10'] = self.posts[row['id']][14]
+                    row['awards_10'] = self.posts[row['id']][15]
+                    row['score_12'] = self.posts[row['id']][16]
+                    row['awards_12'] = self.posts[row['id']][17]
+                    row['score_14'] = self.posts[row['id']][18]
+                    row['awards_14'] = self.posts[row['id']][19]
+                    row['score_16'] = self.posts[row['id']][20]
+                    row['awards_16'] = self.posts[row['id']][21]
+                    row['score_18'] = self.posts[row['id']][22]
+                    row['awards_18'] = self.posts[row['id']][23]
+
                     row['updated'] = self.posts[row['id']][-1]
-                row = {'id':row['id'], 'subreddit':row['subreddit'], 'created':row['created'], 'title':row['title'], 'text':row['text'], 'score':row['score'], 'url':row['url'], 'awards':row['awards'], 'updated':row['updated']}
+                    row = {'id':row['id'], 'subreddit':row['subreddit'], 'created':row['created'], 'title':row['title'], 'text':row['text'], 'score':row['score'], 'url':row['url'], 'awards':row['awards'], 'score_2': row['score_2'], 'awards_2': row['awards_2'], 'score_4': row['score_4'], 'awards_4': row['awards_4'], 'score_6': row['score_6'], 'awards_6': row['awards_6'], 'score_10': row['score_10'], 'awards_10': row['awards_10'], 'score_12':row['score_12'], 'score_14': row['score_14'], 'awards_12': row['awards_12'], 'awards_14': row['awards_14'], 'score_16': row['score_16'], 'awards_16':row['awards_16'], 'score_18': row['score_18'], 'awards_18': row['awards_18'],'updated':row['updated']}
                 writer.writerow(row)
 
         shutil.move(tempfile.name, self.filename)
@@ -135,8 +178,8 @@ class RedditParser(object):
 
 
 if __name__=="__main__":
-    subreddits = ["dadjokes", "todayilearned","askreddit"]
-    FILENAME = "reddit_posts.csv"
+    subreddits = ["dadjokes", "todayilearned","askreddit","showerthoughts"]
+    FILENAME = "/root/NLP/NLP_Reddit/reddit_posts_18.csv"
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', dest='model', action='store_const', const=1, default=0, help='Run the NLP model based off previously gathered data')
     args = parser.parse_args()
